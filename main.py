82109f01c25c48995bbb7aaf8658f47f9172d0c2
Bogdan <145839576+bogdan228Q@users.noreply.github.com>
2026-04-28 22:11:31 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,36 @@
+import sounddevice as sd
+import numpy as np
+import scipy.io.wavfile as wav
+import speech_recognition as sr
+from googletrans import Translator
+
+duration = 5  # секунды записи
+sample_rate = 44100
+
+print("Говори...")
+recording = sd.rec(
+  int(duration * sample_rate), # длительность записи в сэмплах
+  samplerate=sample_rate,      # частота дискретизации
+  channels=1,                  # 1 — это моно
+  dtype="int16")               # формат аудиоданных
+sd.wait()  # ждём завершения записи
+
+wav.write("output.wav", sample_rate, recording)
+print("Запись завершена, теперь распознаём...")
+
+recognizer = sr.Recognizer()
+with sr.AudioFile("output.wav") as source:
+    audio = recognizer.record(source)
+
+try:
+    lang = input("Введите код языка для перевода (например, 'en' — английский, 'es' — испанский): ")
+    text = recognizer.recognize_google(audio, language="ru-RU")
+    translator = Translator()
+    translated = translator.translate(text, dest='en')  # здесь 'en' — это английский
+    print("🌍 Перевод на выбранный язык:", lang, translated.text)
+    print("Ты сказал:", text)
+
+except sr.UnknownValueError:             # - если Google не понял речь (шум, молчание)
+    print("Не удалось распознать речь.")
+except sr.RequestError as e:             # - если нет интернета или API недоступен
+    print(f"Ошибка сервиса: {e}")
